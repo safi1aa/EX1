fae3cf2054d785a04320a527a56e3b3d814aaa12
User <egemberdievasafia@gmail.com>
2023-05-08 19:56:28 +0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("азамат");
+        System.out.println("      ойногону");
+        System.out.println("              кетти ");
+    }
+}
